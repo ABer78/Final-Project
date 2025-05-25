fdb0930d07e464151a101da4152a8282840b2239
Jenna Kurian <jenkur28@bergen.org>
2025-05-25 17:34:02 -0400

    added script and question screen

---
+++ script.js
@@ -0,0 +1,8 @@
+// switching to question screen
+
+function startQuiz() {
+    document.getElementById("welcome").classList.remove("active");
+    document.getElementById("welcome").classList.add("screen");
+
+    document.getElementById("question").classList.add("active");
+}

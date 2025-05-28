33f438e7ebddc764a8f09819390a4476e26e09dc
Arpine Berberyan <arpiberb@gmail.com>
2025-05-28 14:50:16 -0400

    hggh
    Co-authored-by: Jenna Kurian <jennak04@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 // switching to question screen
-
 function startQuiz() {
     document.getElementById("welcome").classList.remove("active");
     document.getElementById("welcome").classList.add("screen");
 
     document.getElementById("question").classList.add("active");
-}
 
-// switch questions
-function nextQue() {
-    
+    // flag setup
+    const countries = {
+        "Armenia": "armenia",
+        "United States of America": "america",
+        "India": "india",
+        "Austria": "austria",
+        "Canada": "canada"
+    };
+
+    const country = "Armenia"; // later you can randomize this
+    const flagUrl = `https://cdn.countryflags.com/thumbs/${countries[country]}/flag-800.png`;
+
+    document.getElementById("flag").src = flagUrl;
+    document.getElementById("flag").alt = `${country} Flag`;
 }
